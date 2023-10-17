ba59fd62d79bae6ba1d616bdaa94fe0a0f8e6e5d
JaewoongHur <jwhur7@naver.com>
2023-10-17 13:47:49 +0900

    :zap: feat: add vite.config.ts paths

--- honey-front/honey-app/vite.config.ts
+++ honey-front/honey-app/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
@@ -7,16 +7,38 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    port : 3000,
+    port: 3000,
   },
   resolve: {
     alias: [
-      {find: "@", replacement: path.resolve(__dirname, "src")},
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
       {
         find: "@assets",
         replacement: path.resolve(__dirname, "src/assets"),
       },
-
-    ]
-  }
-})
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@config",
+        replacement: path.resolve(__dirname, "src/config"),
+      },
+      {
+        find: "@pages",
+        replacement: path.resolve(__dirname, "src/pages"),
+      },
+      {
+        find: "@styles",
+        replacement: path.resolve(__dirname, "src/styles"),
+      },
+      {
+        find: "@types",
+        replacement: path.resolve(__dirname, "src/types"),
+      },
+    ],
+  },
+});
