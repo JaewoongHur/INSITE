d2d6a73d2aac072995caee58c78cc5e6033c3830
YS <dbtjq1592000@gmail.com>
2023-10-17 17:53:36 +0900

    :sparkles: feat : btn 생성

---
+++ honey-front/honey-app/src/components/common/button/TextButton.tsx
@@ -0,0 +1,12 @@
+function TextButton() {
+  return (
+    <button
+      type="button"
+      className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full"
+    >
+      버튼
+    </button>
+  );
+}
+
+export default TextButton;
