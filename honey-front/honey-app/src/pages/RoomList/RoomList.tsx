134fd12d535cb5a0b3b0088c39fc9c86119815a2
JaewoongHur <jwhur7@naver.com>
2023-10-18 17:13:46 +0900

    :zap: feat: add modal component

--- honey-front/honey-app/src/pages/RoomList/RoomList.tsx
+++ honey-front/honey-app/src/pages/RoomList/RoomList.tsx
@@ -1,23 +1,15 @@
-import Modal from "@components/common/modal/Modal";
-import { useState } from "react";
-import TextButton from "@components/common/button/TextButton";
+// import TextButton from "@components/common/button/TextButton";
+import SearchRoom from "@components/search";
+import { useRecoilValue } from "recoil";
+import roomAtoms from "@recoil/roomAtoms";
 
 function RoomList() {
-  const [open, setOpen] = useState<boolean>(false);
+  const nowText = useRecoilValue(roomAtoms);
+
   return (
     <>
-      <div>
-        방 목록
-        <TextButton
-          text="모달버튼"
-          fontColor="1"
-          className=""
-          onClick={() => setOpen((p) => !p)}
-        />
-      </div>
-      <Modal openModal={open} position="fixed" posX="0" posY="0" center>
-        모달
-      </Modal>
+      <SearchRoom />
+      <div>{nowText}</div>
     </>
   );
 }
