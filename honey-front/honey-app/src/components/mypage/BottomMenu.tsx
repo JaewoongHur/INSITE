eaddb9aa6c892d4288776c2a798050857def8c0d
Yuseop <dbtjq1592000@gmail.com>
2023-10-29 22:27:03 +0900

    :zap: feat: 방 수정, 메세지 읽기 완료

--- honey-front/honey-app/src/components/mypage/BottomMenu.tsx
+++ honey-front/honey-app/src/components/mypage/BottomMenu.tsx
@@ -8,7 +8,7 @@ import {
   selectedRoomState,
 } from "@recoil/atom";
 import { useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { greedyPooh, sendPot } from "@assets/images";
 
 function ButtomMenu() {
@@ -21,7 +21,6 @@ function ButtomMenu() {
   const [memberOpen, setMemberOpen] = useState<boolean>(false);
   const [memberList] = useRecoilState<UserType[]>(memberListState);
   const [, setSelectedMember] = useRecoilState<UserType>(selectedMemberState);
-  const nowRoom: RoomType = useRecoilValue<RoomType>(selectedRoomState);
 
   function showMemberList(): void {
     console.log(selectedRoom.id);
@@ -33,7 +32,10 @@ function ButtomMenu() {
   }
 
   function roomPaste(): void {
-    console.log(nowRoom);
+    const link = `http://localhost:3000/room/participate/${selectedRoom.id}`;
+    console.log(link);
+    navigator.clipboard.writeText(link);
+    alert("링크가 클립보드에 복사되었습니다.");
   }
 
   function exitRoom(): void {
