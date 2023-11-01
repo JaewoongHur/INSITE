fa12a60c9b4609bf0a97bf238a4e7d08defea52b
YS <dbtjq1592000@gmail.com>
2023-11-02 08:18:03 +0900

    :zap: feat: 로그인 구현 + store 입력

--- insite-app/src/main.tsx
+++ insite-app/src/main.tsx
@@ -6,10 +6,14 @@ import GlobalStyles from "@assets/styles/globalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "@assets/styles/colors";
 import "@config/axios.config";
+import { Provider } from "react-redux";
+import store from "@store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <RouterProvider router={router} />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </Provider>,
 );
