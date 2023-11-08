a27a93a22594ab9f7a640de40055a9316050a0ab
YS <dbtjq1592000@gmail.com>
2023-11-08 17:34:48 +0900

    :zap: feat: 클릭카운트 더미데이터 삭제

--- insite-app/src/components/button/ClickCount.tsx
+++ insite-app/src/components/button/ClickCount.tsx
@@ -11,44 +11,6 @@ function ClickCount() {
     (state: RootState) => state.SelectedItemInfo.selectedButton,
   );
   const [data, setData] = useState<ButtonType[]>([]);
-  // const [seriesData, setSeriesData] = useState<[number, number][]>();
-  // const dummy = [
-  //   {
-  //     id: 0,
-  //     counts: 100,
-  //     date: "2023-11-03",
-  //   },
-  //   {
-  //     id: 0,
-  //     counts: 5,
-  //     date: "2023-11-04",
-  //   },
-  //   {
-  //     id: 0,
-  //     counts: 5,
-  //     date: "2023-11-08",
-  //   },
-  //   {
-  //     id: 1,
-  //     counts: 6,
-  //     date: "2023-11-09",
-  //   },
-  //   {
-  //     id: 2,
-  //     counts: 15,
-  //     date: "2023-11-10",
-  //   },
-  //   {
-  //     id: 3,
-  //     counts: 1,
-  //     date: "2023-11-11",
-  //   },
-  //   {
-  //     id: 4,
-  //     counts: 50,
-  //     date: "2023-11-12",
-  //   },
-  // ];
 
   useEffect(() => {
     const getDetailData = async () => {
